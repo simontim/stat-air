70c78d1b6b260d50b9da89cf53d696ce6a64278f
Simone Mastrodonato <simone.mastrodonato@telecomitalia.it>
2018-03-06 17:24:59 +0100

    Create hello.java

---
+++ src/main/webapp/hello.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      for(int i = 10; i<10;i++){
+          System.out.println("Hello, World");
+          }
+   }
+}
